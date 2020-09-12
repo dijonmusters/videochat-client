e7c74fb0b0f72cbb9167517fd9df5db8a210f951
Jon Meyers <jonathon.d.meyers@gmail.com>
2020-09-12 14:46:03 +1000

    add turn creds to ice connection

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,6 +1,11 @@
 const config = {
   iceServers: [
     {
+      urls: ['turn:numb.viagenie.ca'],
+      username: 'jonathon.d.meyers@gmail.com',
+      credential: 'fj8KAARSAZ5S'
+    },
+    {
       urls: ['stun:stun.l.google.com:19302'],
     },
   ],
